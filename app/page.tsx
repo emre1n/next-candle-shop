6ed1ebeb581968659904bdab0f343f6459bbbeb1
emre1n <emre1n@protonmail.com>
2023-10-13 18:49:36 +0300

    add images

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-between p-12">
       <button className="btn btn-primary">Button</button>
     </main>
   );
---
+++ components/layout/Navbar/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Navbar() {
+  return (
+    <div className="navbar bg-neutral text-neutral-content">
+      <a className="btn btn-ghost normal-case text-xl">Candle Enchantment</a>
+    </div>
+  );
+}
+
+export default Navbar;
