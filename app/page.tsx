22c330692fffde25567575dd71c8d3431cc4f780
emre1n <emre1n@protonmail.com>
2023-10-13 19:30:05 +0300

    add hero section

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
+import Hero from '@/components/home/Hero';
 import Image from 'next/image';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-12">
-      <button className="btn btn-primary">Button</button>
+    <main className="flex min-h-screen flex-col items-center justify-between">
+      <Hero />
     </main>
   );
 }
