28bd83b942419bc18b33d55aaa039aa7db1612dc
emre1n <emre1n@protonmail.com>
2023-10-13 19:40:39 +0300

    update hero title and subtitle

--- components/home/Hero/index.tsx
+++ components/home/Hero/index.tsx
@@ -5,14 +5,14 @@ function Hero() {
     <div className="hero min-h-screen hero-background">
       <div className="hero-overlay bg-opacity-50"></div>
       <div className="hero-content text-center text-neutral-content">
-        <div className="max-w-md">
-          <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
+        <div className="max-w-xl">
+          <h1 className="mb-5 text-5xl font-bold">
+            Awaken Your Emotions with Unique Candles
+          </h1>
           <p className="mb-5">
-            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
-            a id nisi.
+            Embrace the Warm Glow of Uniquely Crafted Memories
           </p>
-          <button className="btn btn-primary">Get Started</button>
+          <button className="btn btn-primary">Make your candle</button>
         </div>
       </div>
     </div>
--- components/layout/Navbar/index.tsx
+++ components/layout/Navbar/index.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 function Navbar() {
   return (
-    <div className="navbar bg-neutral text-neutral-content">
-      <a className="btn btn-ghost normal-case text-xl">Candle Enchantment</a>
+    <div className="navbar fixed bg-neutral text-neutral-content">
+      <a className="btn btn-ghost normal-case text-xl">Unique Candles</a>
     </div>
   );
 }
