5b72a485bddd6272ceb060160f71ed5cf8148a41
emre1n <emre1n@protonmail.com>
2023-10-27 15:14:33 +0300

    create item model

---
+++ libs/models/item.model.ts
@@ -0,0 +1,34 @@
+export interface ItemType {
+  id: number;
+  attributes: {
+    name: string;
+    imagePath: string;
+    description: string;
+    createdAt: string;
+    updatedAt: string;
+    publishedAt: string;
+    image: {
+      data: {
+        id: number;
+        attributes: {
+          name: string;
+          alternativeText: string | null;
+          caption: string | null;
+          width: number | null;
+          height: number | null;
+          formats: Record<string, any> | null;
+          hash: string;
+          ext: string;
+          mime: string;
+          size: number;
+          url: string;
+          previewUrl: string | null;
+          provider: string;
+          provider_metadata: any | null;
+          createdAt: string;
+          updatedAt: string;
+        };
+      };
+    };
+  };
+}
