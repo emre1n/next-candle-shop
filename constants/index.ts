653307ecf78af7e5a8629816e82da6456b1f6cd2
emre1n <emre1n@protonmail.com>
2023-10-30 16:31:19 +0300

    refactor OrderDetails context

---
+++ constants/index.ts
@@ -0,0 +1,4 @@
+export const pricePerItem = {
+  cups: 10,
+  fragrances: 5,
+};
--- contexts/OrderDetails.tsx
+++ contexts/OrderDetails.tsx
@@ -1,14 +1,42 @@
 import { createContext, useContext, useState } from 'react';
+import { pricePerItem } from '../constants';
 
-interface OrderDetailsContextType {
-  cup: { [cupName: string]: number }[];
-  fragrances: { [fragranceName: string]: number }[];
-}
+type TOrderDetailsContext = {
+  optionCounts: TOptionCounts;
+  totals: {
+    cups: number;
+    fragrances: number;
+  };
+  updateItemCount: (
+    itemName: string,
+    newItemCount: number,
+    optionType: TOptionType
+  ) => void;
+  resetOrder: () => void;
+};
 
-const OrderDetails = createContext<OrderDetailsContextType | null>(null);
+type TOptionCounts = {
+  cups: { [key: string]: number }; // example: { Charcoal: 1, Cream: 2 }
+  fragrances: { [key: string]: number }; // example: { Cedar: 1 }
+};
 
-// Purpose of the custom hook: Create custom hook to check whether we're in a provider
-export function useOrderDetails() {
+type TOptionType = 'cups' | 'fragrances';
+
+const OrderDetails = createContext<TOrderDetailsContext>({
+  optionCounts: {
+    cups: {},
+    fragrances: {},
+  },
+  totals: {
+    cups: 0,
+    fragrances: 0,
+  },
+  updateItemCount: () => {},
+  resetOrder: () => {},
+});
+
+// create custom hook to check whether we're in a provider
+export const useOrderDetails = () => {
   const contextValue = useContext(OrderDetails);
 
   if (!contextValue) {
@@ -16,69 +44,57 @@ export function useOrderDetails() {
       'useOrderDetails must be called from within an OrderDetailsProvider'
     );
   }
-
   return contextValue;
-}
+};
 
-interface OrderDetailsProviderProps {}
+type TProps = {
+  children: React.ReactNode;
+};
 
-export function OrderDetailsProvider(Props: OrderDetailsProviderProps) {
-  const [optionDetails, setOptionDetails] = useState<OrderDetailsContextType>({
-    cup: [], // example: [{'Charcoal': 10}]
-    fragrances: [], // example: [{'Cedar': 5}, {'Coconut': 5}]
+export const OrderDetailsProvider = ({ children }: TProps) => {
+  const [optionCounts, setOptionCounts] = useState<TOptionCounts>({
+    cups: {}, // example: { Charcoal: 1, Cream: 2 }
+    fragrances: {}, // example: { Cedar: 1 }
   });
 
-  function updateItemDetails(
-    newItemDetails: { [itemName: string]: number }[],
-    optionType: 'cup' | 'fragrances'
-  ) {
+  const updateItemCount = (
+    itemName: string,
+    newItemCount: number,
+    optionType: TOptionType
+  ) => {
     // make a copy of existing state
-    const newOptionDetails = { ...optionDetails };
+    const newOptionCounts = { ...optionCounts };
 
     // update the copy with the new information
-    newOptionDetails[optionType] = newItemDetails;
+    newOptionCounts[optionType][itemName] = newItemCount;
 
     // update the state with the updated copy
-    setOptionDetails(newOptionDetails);
-  }
+    setOptionCounts(newOptionCounts);
+  };
 
-  function resetOrder() {
-    setOptionDetails({
-      cup: [], // example: ['Charcoal']
-      fragrances: [], // example: ['Cedar', 'Coconut']
-    });
-  }
+  const resetOrder = () => {
+    setOptionCounts({ cups: {}, fragrances: {} });
+  };
 
   // utility function to derive totals from optionCounts state value
-  function calculateTotal(optionType: 'cup' | 'fragrances') {
-    if (optionType === 'cup') {
-      return itemPrice;
-    } else if (optionType === 'fragrances') {
-      return optionDetails.fragrances.length * itemPrice;
-    }
-  }
+  const calculateTotal = (optionType: TOptionType) => {
+    // get an array of counts for the option type (for example, [1, 2])
+    const countsArray = Object.values(optionCounts[optionType]);
+
+    // total the values in the array of counts for the number of items
+    const totalCount = countsArray.reduce((total, value) => total + value, 0);
+
+    // multiply the total number of items by the price for this item type
+    return totalCount * pricePerItem[optionType];
+  };
 
   const totals = {
-    cup: calculateTotal('cup'),
+    cups: calculateTotal('cups'),
     fragrances: calculateTotal('fragrances'),
   };
 
-  interface ValueType extends OrderDetailsContextType {
-    updateItemDetails: (
-      newItemDetails: string[],
-      optionType: 'cup' | 'fragrances'
-    ) => void;
-    resetOrder: () => void;
-    totals: typeof totals;
-  }
-
-  const value: ValueType = {
-    cup: optionDetails.cup,
-    fragrances: optionDetails.fragrances,
-    totals,
-    updateItemDetails,
-    resetOrder,
-  };
-
-  return <OrderDetails.Provider value={value} {...Props} />;
-}
+  const value = { optionCounts, totals, updateItemCount, resetOrder };
+  return (
+    <OrderDetails.Provider value={value}>{children}</OrderDetails.Provider>
+  );
+};
